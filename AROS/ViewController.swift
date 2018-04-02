2733f5162a7f8047b9f56cc5943c5556076e642e
Zura Mestiashvili <prosperrriii@gmail.com>
2018-04-01 22:40:05 -0400

    Initial commit

---
+++ AROS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AROS
+//
+//  Created by Zura Mestiashvili on 4/1/18.
+//  Copyright © 2018 Zura Mestiashvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
